af63d71997812d61a01f5f83992c3887668a5895
Yogesh Vashisth <writetokhair@gmail.com>
2024-06-01 06:07:00 +0530

    final

--- android/app/src/main/java/com/skylearn/MainActivity.kt
+++ android/app/src/main/java/com/skylearn/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "skylearn"
+  override fun getMainComponentName(): String = "Sky Learns"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
